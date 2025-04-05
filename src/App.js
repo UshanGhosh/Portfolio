ae81d1cb5fc3a00161554f4f67d68802bc93add6
Ushan <ushan@Ushans-MacBook-Air.local>
2025-04-06 01:21:49 +0600

    Added icons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
 import Achievements from './components/Achievements';
 import Skills from './components/Skills';
 import OnlineProfiles from './components/OnlineProfiles';
